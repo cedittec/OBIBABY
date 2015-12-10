75d2803fd3c130b62536364bfcc1cbb1bd22c5a5
Tlahuica <tlahuicadev@gmail.com>
2015-12-10 16:13:59 -0600

    j

---
+++ updateNoise.py
@@ -0,0 +1,13 @@
+import json
+import grovepi
+import time
+import pycurl
+import os
+
+sound_sensor = 5
+fecha = time.strftime('%Y-%m-%d %H:%M:%S')
+noise = grovepi.digitalRead(sound_sensor)
+
+print "sonido "+ str(noise)
+
+#Here we can upload that data, as soon as they get a method.

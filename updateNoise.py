5cafb34c034308a352391259c64e64ba5dae0cff
Tlahuica <tlahuicadev@gmail.com>
2015-12-11 13:08:30 -0600

    Comentario

--- updateNoise.py
+++ updateNoise.py
@@ -11,3 +11,6 @@ noise = grovepi.analogRead(sound_sensor)
 print "sonido "+ str(noise)
 
 #Here we can upload that data, as soon as they get a method.
+
+
+#raspivid --verbose --nopreview -hf -vf --width 640 --height 480 --framerate 15 --bitrate 1000000 --profile baseline --timeout 0 -o - | gst-launch-0.10 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host=104.131.33.240 port=8004 alsasrc device=plughw:Set ! audioconvert ! audioresample ! opusenc ! rtpopuspay ! udpsink host=104.131.33.240 port=8005

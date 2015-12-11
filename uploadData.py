a1b0db92054613f1ee2bc753c4d18a7cb208df18
Tlahuica <tlahuicadev@gmail.com>
2015-12-11 15:53:27 -0600

    uploadData

--- uploadData.py
+++ uploadData.py
@@ -12,59 +12,22 @@ except ImportError:
 import time
 import os
 
-def logMov(time,fecha):
-	#Url de la pagina en la que se hace el registro... 01 800 099 0316   -> 305   
-	log_url = 'http://obibaby.com/api/v1/account/logs/temperatures'
-	#JSON que se envia...
-	data = json.dumps({
-		"type": "log_user_motion",
-		"data": {
-			"user_id": "2",
-			"time": "50",
-			"updated_at": fecha,
-			"created_at": fecha,
-			"id": 2
-		},
-		"relationships": {
-			"user": {
-				"id": "2",
-				"name": "Erick",
-				"last_name": "",
-				"email": "erick2@csgroup.mx",
-				"telephone": "",
-				"baby_photo": "",
-				"baby_name": "",
-				"baby_birthdate": "",
-				"baby_sex": "null",
-				"baby_weight": "0",
-				"baby_height": "0",
-				"created_at": "2015-08-17 16:12:03",
-				"updated_at": "2015-08-17 16:13:32",
-				"deleted_at": "null"
-			}
-		}
-	})
 
-	c = pycurl.Curl()
-	c.setopt(pycurl.URL, log_url)
-	#autenticacion...
-	c.setopt(pycurl.USERPWD, "%s:%s" % ('test@obibaby.com', '12345678'))
-	c.setopt(pycurl.HTTPHEADER, ["Accept: application/json"])
-	c.setopt(pycurl.POST, 1)
-	c.setopt(pycurl.POSTFIELDS, data)
-	c.perform()
 
 #Registro de temperatura...
 def logTemp(temperature,humidity,fecha):
 	#Url de la pagina en la que se hace el registro...
 	log_url = 'http://obibaby.com/api/v1/account/logs/temperatures'
+	
+	data = 'temperature='+str(temperature)+'&humidity='+str(humidity)
 	#JSON que se envia...
+	'''
 	data = json.dumps({
 		"type": "log_user_temperatures",
 		"data": {
-			"value": humidity,
-			"pollution": temperature,
-			"user_id": "2",
+			"temperature": temperature,
+    		"humidity": humidity,
+    		"user_id": "2",
 		    "updated_at": fecha,
 			"created_at": fecha,
 			"id": 5
@@ -88,7 +51,7 @@ def logTemp(temperature,humidity,fecha):
 	  		}
 	  	}
 	})
-
+	'''
 	c = pycurl.Curl()
 	c.setopt(pycurl.URL, log_url)
 	#autenticacion...

959d7200a204cc9e905fbb9e245ef1dd28b16fb9
Tlahuica <ArielPosada@Ariels-MacBook-Pro.local>
2015-12-15 11:20:43 -0600

    Cartucho pa' tu caucho

--- def/client-ubuntu.py
+++ def/client-ubuntu.py
@@ -12,7 +12,9 @@ def internet_on():
         try:
                 response=urllib2.urlopen('http://74.125.228.100',timeout=1)
                 return True
-        except urllib2.URLError as err: pass
+        except: 
+                return False
+                #urllib2.URLError as err: pass
         return False
 
 def add_to_file(network_data):

93803e9cd2e91a11508a5957ea316b7442c61508
Tlahuica <ArielPosada@Ariels-MacBook-Pro.local>
2015-12-16 17:24:19 -0600

    Order

--- install.sh
+++ install.sh
@@ -27,16 +27,10 @@ git clone 'https://github.com/SelfDestroyer/pyMOD-OLED.git' /home/pi/Desktop/tmp
 cd /home/pi/Desktop/tmp/pyMOD-OLED
 python setup.py install
 
-echo "Clonando el proyecto del github de Cedittec"
-#"Se procederá a ir a una carpeta temporal para descargar los archivos de obibaby"
-git clone ‘https://github.com/cedittec/obibaby’ /home/pi/Desktop/obibaby
 
 
 #"------------------------------------------------------------------------------"
 echo "Instalación del video streaming"
-
-sudo apt-get update -y
-sudo apt-get upgrade -y
 
 #"Se instalarán dependencias del video streaming"
 sudo apt-get install libgstreamer0.10-0 libgstreamer0.10-dev gstreamer0.10-tools gstreamer0.10-plugins-base libgstreamer-plugins-base0.10-dev gstreamer0.10-plugins-good gstreamer0.10-plugins-ugly gstreamer0.10-plugins-bad libgstreamer-plugins-base1.0-dev -y
@@ -47,6 +41,10 @@ echo "Instalacion de paquetería Wireless"
 #"Este paquete permite conexión a una red dada el SSID key password"
 sudo pip install wireless 
 
+
+echo "Clonando el proyecto del github de Cedittec"
+#"Se procederá a ir a una carpeta temporal para descargar los archivos de obibaby"
+git clone ‘https://github.com/cedittec/obibaby’ /home/pi/Desktop/obibaby
 
 #Se tienen que copiar los archivos que pasan cada comando via terminal
 echo "Copiando archivos de rutinas a /bin"

af85afa7eb6a843e788b1048a5faec94f1f03b35
Tlahuica <ArielPosada@MacBook-Pro.local>
2016-01-06 10:47:49 -0600

    More logs

--- def/main.py
+++ def/main.py
@@ -29,14 +29,18 @@ if (internet_off()):
 
 	for item in list:
 		try:
+			os.system("echo $(date '+%Y/%M/%D %H:%M:%S')-> Red: "+item+" >> /home/pi/Desktop/log.txt")
 			print item
 			wireless = Wireless()
 			bssid,psk = item.split(",")
 			print("ssid: "+bssid+ " pass: "+ psk)
 			wireless.connect(ssid=bssid, password=psk)
+			os.system("echo $(date '+%Y/%M/%D %H:%M:%S')-> Red: Intento >> /home/pi/Desktop/log.txt")
 			if (internet_on()):
 				print("Conected")
 				break
+			os.system("echo $(date '+%Y/%M/%D %H:%M:%S')-> Red: No conectado >> /home/pi/Desktop/log.txt")
+			os.system("echo '' >> /home/pi/Desktop/log.txt")
 		except:
 			print "Not connected or invalid input, mi cuate"
 			continue

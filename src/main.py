c527de6ba974df736a7e57b693ea31ef30f253e9
Tlahuica <ArielPosada@MacBook-Pro.local>
2016-01-08 13:58:54 -0600

    Cuatro

--- src/main.py
+++ src/main.py
@@ -17,33 +17,7 @@ def internet_on():
                 conn.request("HEAD", "/")
                 return True
         except:
-                return False
-
-if (internet_off()):
-	file = open('/home/pi/Desktop/networks.txt', 'r')
-	text = file.read()
-	list = text.split('\n')
-
-	for item in list:
-		try:
-			os.system("echo \"$(date '+%Y/%M/%D %H:%M:%S')-> Red: "+item+"\" >> /home/pi/Desktop/log.txt")
-			print item
-			os.system("echo \"$(date '+%Y/%M/%D %H:%M:%S')-> Antes del wireless\" >> /home/pi/Desktop/log.txt")
-			wireless = Wireless()
-			bssid,psk = item.split(",")
-			print("ssid: "+bssid+ " pass: "+ psk)
-			wireless.connect(ssid=bssid, password=psk)
-			os.system("echo \"$(date '+%Y/%M/%D %H:%M:%S')-> Red: Intento\" >> /home/pi/Desktop/log.txt")
-			if (internet_on()):
-				print("Conected")
-				os.system("echo \"$(date '+%Y/%M/%D %H:%M:%S')-> Red: Conectado\" >> /home/pi/Desktop/log.txt")
-				os.system("echo '' >> /home/pi/Desktop/log.txt")
-				break
-			os.system("echo \"$(date '+%Y/%M/%D %H:%M:%S')-> Red: No conectado\" >> /home/pi/Desktop/log.txt")
-			os.system("echo '' >> /home/pi/Desktop/log.txt")
-		except:
-			print "Not connected or invalid input, mi cuate"
-			continue
+                return False	
 
 while(True):
 	if (internet_off()):
@@ -53,17 +27,24 @@ while(True):
 
 		for item in list:
 			try:
+				os.system("echo \"$(date '+%Y/%M/%D %H:%M:%S')-> Red: "+item+"\" >> /home/pi/Desktop/log.txt")
 				print item
+				os.system("echo \"$(date '+%Y/%M/%D %H:%M:%S')-> Antes del wireless\" >> /home/pi/Desktop/log.txt")
 				wireless = Wireless()
 				bssid,psk = item.split(",")
 				print("ssid: "+bssid+ " pass: "+ psk)
 				wireless.connect(ssid=bssid, password=psk)
+				os.system("echo \"$(date '+%Y/%M/%D %H:%M:%S')-> Red: Intento\" >> /home/pi/Desktop/log.txt")
 				if (internet_on()):
 					print("Conected")
+					os.system("echo \"$(date '+%Y/%M/%D %H:%M:%S')-> Red: Conectado\" >> /home/pi/Desktop/log.txt")
+					os.system("echo '' >> /home/pi/Desktop/log.txt")
 					break
+				os.system("echo \"$(date '+%Y/%M/%D %H:%M:%S')-> Red: No conectado\" >> /home/pi/Desktop/log.txt")
+				os.system("echo '' >> /home/pi/Desktop/log.txt")
 			except:
 				print "Not connected or invalid input, mi cuate"
-			continue
+				continue
 
 	if (internet_off()):
 		os.system("echo \"$(date '+%Y/%M/%D %H:%M:%S')-> Red: Iniciando daemon bluetooth\" >> /home/pi/Desktop/log.txt")
